32f1963a648133b972f800569da9e4d4c180cf76
Guestie808 <tyler.guest@hotmail.co.uk>
2026-03-20 22:51:37 +0000

    V18.2
    - More misc changes
    - Added empty plan.html page with working nav-column
    - Next to add: Togglable filter buttons, potentially added on map page too for fine tuning pins

---
+++ documentation templates/docs/PubCrawlPlanner/common.js
@@ -0,0 +1,18 @@
+function setColumnActive(button) {
+    document.querySelectorAll('.nav-column .nav-button').forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+}
+
+// Request user geolocation and callback with lat, lon
+function getLocation(page) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition( // permission granted, denied - location defaults to central Bristol
+            loc => window.location.href=`${page}?lat=${loc.coords.latitude}&lon=${loc.coords.longitude}`,
+            () => window.location.href=`${page}?lat=51.454514&lon=-2.587910`
+        )
+    }
+    else { // if unsupported feature, location defaults to central Bristol
+        window.alert("Geolocation is not supported by this browser.");
+        window.location.href=`${page}?lat=51.454514&lon=-2.587910`;
+    }
+}
--- documentation templates/docs/PubCrawlPlanner/index.js
+++ documentation templates/docs/PubCrawlPlanner/index.js
@@ -54,20 +54,6 @@ function search(searchTerm) { // shows fields with partial match to search input
     document.querySelectorAll('.nav-bar .nav-button').forEach(btn => btn.classList.remove('active'));
 }
 
-// Request user geolocation and callback with lat, lon
-function getLocation(page) {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition( // permission granted, denied - location defaults to central Bristol
-            loc => window.location.href=`${page}?lat=${loc.coords.latitude}&lon=${loc.coords.longitude}`,
-            () => window.location.href=`${page}?lat=51.454514&lon=-2.587910`
-        )
-    }
-    else { // if unsupported feature, location defaults to central Bristol
-        window.alert("Geolocation is not supported by this browser.");
-        window.location.href=`${page}?lat=51.454514&lon=-2.587910`;
-    }
-}
-
 function setBarActive(button) {
     document.querySelectorAll('.nav-bar .nav-button').forEach(btn => btn.classList.remove('active'));
     button.classList.add('active');
